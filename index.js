b5d86a8385c64a68599babd8628a07488daf127b
laila624 <superherrro624@gmail.com>
2024-02-04 16:51:52 +0300

    hw

---
+++ index.js
@@ -0,0 +1,20 @@
+//Task 1 :
+var myName = 'laila ';
+var myAge = 22 ;
+var isStudent = true ;
+
+console.log(myName);
+console.log(myAge);
+console.log(isStudent);
+
+//Task 2 :
+let year = 2024 ;
+
+console.log(typeof year);
+
+//Task 3 :
+let greet = 'Hello ';
+let name ='laila ';
+
+console.log(greet + 'my name is laila ' + name + ' bye ' );
+
